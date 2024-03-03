3c97236463c4d9a8eeaa6e435f5eb531bddac3de
Metin Özkaya <klombe@outlook.dk>
2024-03-03 23:54:34 +0100

    add user post table to database and change out dummy data to use it instead in templates

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        "author": "KLombe",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "February 28, 2018"
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blog Post 2",
-        "content": "Second post content",
-        "date_posted": "February 27^n, 2018"
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 

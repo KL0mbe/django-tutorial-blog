29d6fff66e48aafe44912bd7edfc2dba6585c567
Metin Özkaya <klombe@outlook.dk>
2024-03-07 23:19:04 +0100

    create registration page and add crispy-forms to it

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def register(requests):
+    if requests.method == "POST":
+        form = UserRegisterForm(requests.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(requests, f"Account Created for {username}!")
+            return redirect("blog-home")
+    else:
+        form = UserRegisterForm()
+    return render(requests, "users/register.html", {"form": form})
+

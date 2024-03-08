cae8a2c658c6a30cba64a2fd92a0fa687d0c83d9
Metin Özkaya <klombe@outlook.dk>
2024-03-08 21:06:35 +0100

    add logout feature to site and update base template navbar to change depending on login/logout status

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import logout
 from .forms import UserRegisterForm
 
 def register(requests):
@@ -8,9 +9,17 @@ def register(requests):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(requests, f"Account Created for {username}!")
-            return redirect("blog-home")
+            messages.success(requests, f"Your account has been Successfully created!")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(requests, "users/register.html", {"form": form})
 
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/logout.html")
+
+
+def profile(request):
+    return render(request, "users/profile.html")

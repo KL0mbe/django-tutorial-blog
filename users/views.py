0699954ccc4c4f6c39a6355b3d34580ca77edf18
Metin Özkaya <klombe@outlook.dk>
2024-03-14 15:26:10 +0100

    update user profile to allow them to change name, email and picture, change home page function view with class view

--- users/views.py
+++ users/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import *
 
-def register(requests):
-    if requests.method == "POST":
-        form = UserRegisterForm(requests.POST)
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(requests, f"Your account has been Successfully created!")
+            messages.success(request, f"Your account has been Successfully created!")
             return redirect("login")
     else:
         form = UserRegisterForm()
-    return render(requests, "users/register.html", {"form": form})
+    return render(request, "users/register.html", {"form": form})
 
 
 def logout_view(request):
@@ -24,4 +25,23 @@ def logout_view(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Your account has been updated!")
+            return redirect("profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        "u_form": u_form,
+        "p_form": p_form
+    }
+
+    return render(request, "users/profile.html", context)
